65957c14df668acf02b8f9fe13b0573d2d30f445
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-22 20:25:14 +0200

    solution-taxi-service

--- taxi/models.py
+++ taxi/models.py
@@ -6,11 +6,15 @@ class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
-
-
 class Car(models.Model):
     model = models.CharField(max_length=255)
     manufacturer = models.ForeignKey(
