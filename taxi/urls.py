bbdfb72fcca9bc05808cade29fac56cf02322e0e
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-01-23 11:54:25 +0200

    solution-taxi-service

--- taxi/urls.py
+++ taxi/urls.py
@@ -18,10 +18,11 @@ urlpatterns = [
         ManufacturerListView.as_view(),
         name="manufacturer-list"
     ),
-    path('manufacturers/<int:pk>/',
-         views.ManufacturerDetailView.as_view(),
-         name='manufacturer-detail'
-         ),
+    path(
+        'manufacturers/<int:pk>/',
+        views.ManufacturerDetailView.as_view(),
+        name='manufacturer-detail'
+    ),
 
     path("cars/", CarListView.as_view(), name="car-list"),
     path("cars/<int:pk>/", CarDetailView.as_view(), name="car-detail"),
